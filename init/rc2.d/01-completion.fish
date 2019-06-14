0dcea850a93561297c91f4d009c7ff1ff97a2e64
Andrew Matthews <andrew.matthews@team.telstra.com.au>
2019-06-14 10:17:56 +1000

    more tinkering

--- init/rc2.d/01-completion.fish
+++ init/rc2.d/01-completion.fish
@@ -1,11 +1,1 @@
-complete -c idea -x -a home -d 'go to the ideas home directory'
-complete -c idea -x -a create  -d 'create a new idea'
-complete -c idea -x -a thought -d 'add a thought that this idea prompts'
-complete -c idea -x -a question -d 'add a question to be answered'
-complete -c idea -x -a test -d 'add a test to be performed'
-complete -c idea -x -a subidea -d 'add a hypothesis'
-complete -c idea -x -a task -d 'add a task to be completed'
-complete -c idea -x -a summarise -d 'bring together everything on one screen'
-complete -c idea -x -a save -d 'save all changes to local git remo'
-complete -c idea -x -a sync -d 'save changes then push ro remote repo'
-complete -c idea -x -a consolidate -d 'bring all changes together in a single file'
+complete -c idea -x -a "home create thought question test subidea task summarise save sync consolidate"
